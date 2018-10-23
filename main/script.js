11dbaba98acfb099969059b8b53e29d86da5a9df
cieslaluk <ciesla.luk@gmail.com>
2018-10-23 18:37:50 +0200

    addedBouncingFeatures

--- main/script.js
+++ main/script.js
@@ -1,22 +1,34 @@
 let canvas;
 let canvasContext;
 let ballPosX = 50;
+let ballSpeedX = 10;
 
 window.onload = function() {
     canvas = document.getElementById('game');
     canvasContext = canvas.getContext('2d');
     
-    let framesPerSec = 30;
-    setInterval(drawAll, 1000/framesPerSec);
+    let framesPerSec = 60;
+    setInterval(function(){
+        drawAll();
+        moveAll()
+    }, 1000/framesPerSec);
 };   
 
-function drawAll() {
-ballPosX = ballPosX + 20;
+function moveAll(){
+    ballPosX = ballPosX + ballSpeedX;
+    if(ballPosX > canvas.width){
+        ballSpeedX = -ballSpeedX;
+    }
+    if(ballPosX < 0){
+        ballSpeedX = -ballSpeedX;
+    }
+};
 
+function drawAll() {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0,canvas.width, canvas.height);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(0, 210, 200, 200)
+    canvasContext.fillRect(20, 210, 10, 100)
     canvasContext.fillStyle = 'red';
     canvasContext.fillRect(ballPosX,100,10,10);
     
