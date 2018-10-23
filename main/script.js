0d2f0d903faa2115d5498912706711585112a409
cieslaluk <ciesla.luk@gmail.com>
2018-10-23 18:03:59 +0200

    addedBallMotion

--- main/script.js
+++ main/script.js
@@ -1,19 +1,24 @@
-var canvas;
-var canvasContext;
+let canvas;
+let canvasContext;
+let ballPosX = 50;
 
 window.onload = function() {
     canvas = document.getElementById('game');
     canvasContext = canvas.getContext('2d');
-    drawAll();
+    
+    let framesPerSec = 30;
+    setInterval(drawAll, 1000/framesPerSec);
 };   
 
 function drawAll() {
+ballPosX = ballPosX + 20;
+
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0,canvas.width, canvas.height);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(225, 210, 200, 200)
+    canvasContext.fillRect(0, 210, 200, 200)
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(canvas.width/2,200,50,25);
+    canvasContext.fillRect(ballPosX,100,10,10);
     
 
 };
