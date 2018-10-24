c5db873e9c47347e5900c5ec4fe930813c7b20f0
cieslaluk <ciesla.luk@gmail.com>
2018-10-24 21:08:46 +0200

    cleaningUpCode

--- main/script.js
+++ main/script.js
@@ -25,12 +25,21 @@ function moveAll(){
 };
 
 function drawAll() {
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0,0,canvas.width, canvas.height);
-    canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(20, 210, 10, 100)
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(ballPosX,100,10,10);
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+    colorRect(20, 210, 10, 100, 'white');
+    colorCircle(ballPosX, 150, 10, 'white');
     
-
 };
+
+function colorCircle(centerX, centerY, radius, drawColor){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
+}
+
+function colorRect(leftX, topY, width, height, drawColor){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+};
+
